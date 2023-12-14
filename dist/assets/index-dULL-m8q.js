60d1dd7eabfe15f92490c829387ad5234b8ed9ab
gileandrocampos <gileandrocampos8@gmail.com>
2023-12-13 21:10:16 -0300

    Change diretory

--- dist/assets/index-dULL-m8q.js
+++ dist/assets/index-dULL-m8q.js
@@ -3,7 +3,7 @@ var g=Object.defineProperty;var w=(e,t,s)=>t in e?g(e,t,{enumerable:!0,configura
       <div class="card__image ${e.type}">
         <img src="${e.image}" alt="Poke">
         <div class="effect__image">
-          <img src="../icons-type/${e.type}.svg" alt="${e.type}">
+          <img src="icons-type/${e.type}.svg" alt="${e.type}">
         </div>
       </div>
       <div class="card__details">
