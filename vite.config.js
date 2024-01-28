60253c058dbc3b9cf52ce1ede0b8c03647b4222f
gileandrocampos <gileandrocampos8@gmail.com>
2024-01-28 08:15:15 -0300

    resolve merge

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     assetsDir: 'img',
-    assetsInclude: ['icons-type/**/*.svg'], // Mantenha este padrão para incluir SVGs em icons-type
   },
 });
