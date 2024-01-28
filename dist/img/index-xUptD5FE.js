e753023e9e570751810fc91e9842df8b8e4ae16a
gileandrocampos <gileandrocampos8@gmail.com>
2024-01-28 08:10:00 -0300

    resolve merge

---
+++ dist/img/index-xUptD5FE.js
@@ -0,0 +1,24 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))a(s);new MutationObserver(s=>{for(const n of s)if(n.type==="childList")for(const r of n.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&a(r)}).observe(document,{childList:!0,subtree:!0});function o(s){const n={};return s.integrity&&(n.integrity=s.integrity),s.referrerPolicy&&(n.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?n.credentials="include":s.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function a(s){if(s.ep)return;s.ep=!0;const n=o(s);fetch(s.href,n)}})();const v=document.querySelector("[ data-js='search-pokemon']"),P=document.querySelectorAll("input[type='checkbox']"),k=document.querySelector("#clearFilter"),f=document.querySelector("#filterTypeButton");let c=[];const u=document.querySelector(".pokedex-list"),L=document.querySelector(".menu__btn-mobile");L.addEventListener("click",e=>{e.currentTarget.classList.toggle("active")});const i=20;let l=0;class _{constructor(t){this.number=t.id,this.name=t.name,this.image=t.sprites.other["official-artwork"].front_default,this.types=t.types.map(o=>o.type.name),this.type=this.types[0]}}const y=e=>new _(e),S=async(e,t)=>{const n=(await(await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${e}&limit=${t}`)).json()).results.map(async r=>{const w=await(await fetch(r.url)).json();return y(w)});return Promise.all(n)},p=async()=>await(await(await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1080")).json()).results,j=async()=>{const a=(await(await(await fetch("https://pokeapi.co/api/v2/type/")).json()).results).map(async s=>await(await fetch(s.url)).json());return console.log(Promise.all(a)),Promise.all(a)};j();const h=async e=>{const o=await(await fetch(e.url)).json();return y(o)},T=async e=>(await p()).filter(a=>a.name.toLowerCase().startsWith(e)),q=async e=>(await p()).filter(a=>a.url.endsWith(`/${e}/`)),$=async e=>{const o=await(await p()).map(h);return(await Promise.all(o)).filter(n=>e.some(r=>r==n.types))};function b(e,t){return e.toString().padStart(t,"0")}const m=e=>`
+      <li class="card cursor-pointer">
+        <div class="card__image ${e.type}">
+          <img src="${e.image}" alt="${e.name}">
+          <div class="effect__image">
+            <img src="icons-type/${e.type}.svg" alt="${e.type}">
+          </div>
+        </div>
+        <div class="card__details">
+          <div class="card__info">
+            <span class="card__number">n.º ${b(e.number,3)}</span>
+            <span class="card__name"> ${e.name}</span>
+          </div>
+          <div class="card__types">
+          ${e.types.map(t=>`
+          <span class="type ${t}">
+            <img src="icons-type/${t}.svg" alt="">
+            ${t}
+          </span>`).join("")}
+            
+          </div>
+        </div>
+      </li>
+    `,E=e=>{const t=u.querySelectorAll(".card__name"),o=Array.from(t).map(s=>s.textContent.trim()),a=e.filter(s=>!o.includes(s.name)).map(m).join("");u.innerHTML+=a},d=async(e,t,o=!1)=>{o&&(e=0,u.innerHTML="");const a=await S(e,t);E(a)};d(l,i,!1);const M=document.querySelector("[data-js='modal-types']"),B=document.querySelector("#close-modal"),H=document.querySelector("#modal"),g=document.querySelector("#fade"),N=()=>{H.classList.toggle("hide"),g.classList.toggle("hide")};[M,B,g,f].forEach(e=>{e.addEventListener("click",()=>N())});function O(e=10){const t=window.scrollY+window.innerHeight,o=document.documentElement.scrollHeight;return t>=o-e}function A(e){if(e.checked==!0&&(c.push(e.dataset.js),console.log(c)),e.checked==!1){let t=c.indexOf(e.dataset.js);c.splice(t,1),console.log(c)}}window.addEventListener("DOMContentLoaded",()=>{document.addEventListener("scroll",()=>{O()&&(l+=i,d(l,i,!1))}),v.addEventListener("input",async e=>{const t=e.currentTarget.value,o=!isNaN(t),a=t.toLowerCase();let s;if(t.trim()==="")return d(l,i,!0);o?s=await q(t):s=await T(a);const n=await Promise.all(s.map(h)),r=n.map(m).join("");console.log(n),u.innerHTML=r}),document.addEventListener("click",e=>{const t=e.target;t.matches("input[type='checkbox']")&&!c.includes(t.dataset.js)&&A(t)}),k.addEventListener("click",()=>{P.forEach(e=>{e.checked=!1,c=[]})}),f.addEventListener("click",async()=>{if(c.length>0){const t=(await $(c)).map(m).join("");u.innerHTML=t}else d(l,i,!0)})});d(l,i,!0);
--- src/main.js
+++ src/main.js
@@ -1,67 +1,5 @@
   import "./input.css"
 
-<<<<<<< HEAD
-// Variaveis
-const inputSearchByPokemon = document.querySelector("[ data-js='search-pokemon']")
-const pokemonList = document.querySelector(".pokedex-list");
-const btnMenuMobile = document.querySelector(".menu__btn-mobile");
-
-// Toggle Menu Mobile
-btnMenuMobile.addEventListener("click", (e) => {
-  e.currentTarget.classList.toggle("active")
-})
-
-// Config Limit and Quantity Pokemons requisitions API
-const limit = 20;
-let offset = 0;
-
-// My Model to Pokemon
-class Pokemon {
-  number;
-  name;
-  image;
-  type;
-  types = [];
-}
-
-const convertDetailsToModelPokemon = (detail) => {
-  const pokemon = new Pokemon();
-  pokemon.name = detail.name;
-  pokemon.number = detail.id;
-  pokemon.image = detail.sprites.other["official-artwork"]["front_default"];
-
-  const types = detail.types.map((typeSlot) => typeSlot.type.name)
-  const [type] = types;
-
-  pokemon.types = types;
-  pokemon.type = type;
-  
-  return pokemon;
-}
-
-const getDetails = async (pokemon) => {
-  const response = await fetch(pokemon.url);
-  const data = await response.json();
-  const modelPokemon = convertDetailsToModelPokemon(data);
-  return modelPokemon;
-}
-
-const getPokemons = async (offset, limit) => {
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
-  const data = await response.json();
-  const pokemons = await data.results;
-  const details = await pokemons.map(getDetails);
-  const detailsRequest = Promise.all(details);
-
-  return detailsRequest;
-}
-
-const filterByName = async (name) => {
-  const allPokemons = await getAllPokemons();
-  const result = allPokemons.filter(pokemon => {
-    return pokemon.name.toLowerCase().startsWith(name)
-=======
   const inputSearchByPokemon = document.querySelector("[ data-js='search-pokemon']")
   const inputSearchByType = document.querySelectorAll("input[type='checkbox']");
   const clearFilterButton = document.querySelector("#clearFilter");
@@ -73,7 +11,6 @@ const filterByName = async (name) => {
   const btnMenuMobile = document.querySelector(".menu__btn-mobile");
   btnMenuMobile.addEventListener("click", (e) => {
     e.currentTarget.classList.toggle("active")
->>>>>>> 4c7d58f65270df0a5301c4358fd7359aef635896
   })
 
   const limit = 20;
@@ -94,36 +31,6 @@ const filterByName = async (name) => {
 
   const convertDetailsToModelPokemon = (detail) => new Pokemon(detail);
 
-<<<<<<< HEAD
-const convertPokemonToCard = (pokemon) => {
-  return `
-    <li class="card">
-      <div class="card__image ${pokemon.type}">
-        <img src="${pokemon.image}" alt="Poke">
-        <div class="effect__image">
-          <img src="img/${pokemon.type}.svg" alt="${pokemon.type}">
-        </div>
-      </div>
-      <div class="card__details">
-        <div class="card__info">
-          <span class="card__number">n.º ${leftFillNum(pokemon.number, 3)}</span>
-          <span class="card__name"> ${pokemon.name}</span>
-        </div>
-        <div class="card__types">
-        ${pokemon.types.map((type) => `
-        <span class="type ${type}">
-          <img src="img/${type}.svg" alt="">
-          ${type}
-        </span>`).join("")}
-          
-        </div>
-      </div>
-    </li>
-  `
-}
-
-async function loadPokemonItens(offset, limit, reloadList = false) {
-=======
   const getPokemons = async (offset, limit) => {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`);
     const data = await response.json();
@@ -134,16 +41,10 @@ async function loadPokemonItens(offset, limit, reloadList = false) {
       const detailsData = await detailsResponse.json();
       return convertDetailsToModelPokemon(detailsData);
     });
->>>>>>> 4c7d58f65270df0a5301c4358fd7359aef635896
 
       return Promise.all(detailsPromises);
   }
 
-<<<<<<< HEAD
-  getPokemons(offset, limit).then((pokemons = []) => {
-    const currentPokemonNames = Array.from(pokemonList.querySelectorAll('.card__name')).map(name => name.textContent.trim());
-    
-=======
 
   const getAllPokemons = async () => {
     const response = await fetch(
@@ -287,7 +188,6 @@ async function loadPokemonItens(offset, limit, reloadList = false) {
     const allCardsName = pokemonList.querySelectorAll(".card__name");
     const currentPokemonNames = Array.from(allCardsName).map(name => name.textContent.trim());
 
->>>>>>> 4c7d58f65270df0a5301c4358fd7359aef635896
     const newHtml = pokemons
       .filter(pokemon => !currentPokemonNames.includes(pokemon.name))
       .map(createCardPokemon)
@@ -324,37 +224,6 @@ async function loadPokemonItens(offset, limit, reloadList = false) {
     el.addEventListener("click", () => toggleModal());
   });
 
-<<<<<<< HEAD
-function userNearBottom(threshold = 10) {
-  const scrollPosition = window.scrollY + window.innerHeight;
-  const documentHeight = document.documentElement.scrollHeight;
-  return scrollPosition >= documentHeight - threshold;
-}
-
-
-window.addEventListener("load", () => {
-
-  document.addEventListener("scroll", () => {
-    if (userNearBottom()) {
-      offset += limit;
-      loadPokemonItens(offset, limit, false);
-    }
-  })
-  
-  inputSearchByPokemon.addEventListener("keyup", async (input) => {
-    const inputValue = input.currentTarget.value;
-    const idPokemon = !isNaN(inputValue);
-    const namePokemon = inputValue.toLowerCase();
-  
-    let result;
-
-    if(inputValue.trim() == "") {
-      return loadPokemonItens(offset, limit, true)
-    } else if (idPokemon) {
-      result = await filterById(inputValue);
-    } else {
-      result = await filterByName(namePokemon);
-=======
 
   function userNearBottom(threshold = 10) {
     const scrollPosition = window.scrollY + window.innerHeight;
@@ -366,7 +235,6 @@ window.addEventListener("load", () => {
     if(input.checked == true) {
       arrTypes.push(input.dataset.js)
       console.log(arrTypes)
->>>>>>> 4c7d58f65270df0a5301c4358fd7359aef635896
     } 
     
     if(input.checked == false){
@@ -437,6 +305,6 @@ window.addEventListener("load", () => {
 
   })
 
-loadPokemonItens(offset, limit, true)
+loadPokemonItems(offset, limit, true)
 
 
